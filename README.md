d40d400f23265a81ce83795aa373701d1e4202bb
ulysses-ck <ulisesapaza.280@gmail.com>
2023-07-20 17:33:00 -0300

    update: index.js

---
+++ README.md
@@ -0,0 +1,5 @@
+# get2post
+A simple api that turns a GET  request into a POST request
+
+## Example
+
--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import express from "express";
 import axios from "axios";
-import FormData from "form-data";
 
 const app = express();
-
 const port = process.env.PORT || 3000;
 
 app.get("/test", (req, res) => {
@@ -12,37 +10,21 @@ app.get("/test", (req, res) => {
 
 app.get("/", (req, res) => {
 	const { url, json } = req.query;
-
-	// parse query param json into an object
 	try {
-		const dataObj = JSON.parse(json);
-
-		// create a new FormData object to simulate a frontend has a form
-		const postFormData = new FormData();
-
-		// iterates through the object's properties and assigns them to postFormData
-		Object.keys(dataObj).forEach((key) => {
-			postFormData.append(key, dataObj[key]);
-		});
-
+		const dataJSON = JSON.parse(json);
 		axios({
-			method: "post",
+			method: "POST",
 			url: url,
-			data: postFormData,
+			data: new URLSearchParams(dataJSON),
 		})
 			.then((response) => {
-				// send POST response
-				const { data } = response;
-				res.send(data);
+				res.send(response.data);
 			})
-			.catch(function (error) {
-				//handle error
-				res.send({ message: "An error occurs", error });
+			.catch((error) => {
+				res.send({ message: "An error happens!", error });
 			});
 	} catch (error) {
-		res.send({
-			message: "An error occurs parsing json query param",
-		});
+		res.send({ message: "Error at parse query param json" });
 	}
 });
 
