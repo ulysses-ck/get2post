e2c7518b13156a92c95530e4f1f74d3d66a129ed
ulysses-ck <ulisesapaza.280@gmail.com>
2023-07-20 00:26:32 -0300

    init project

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get("/api/", (req, res) => {
+	let json = JSON.stringify(req.query.json);
+	console.log(req.query.url);
+	console.log(req.query.json);
+	axios.post(req.query.url, json).then(
+		(response) => {
+			res.send(response.data);
+		},
+		(error) => {
+			res.send(error);
+		}
+	);
+});
+
+app.listen(port, () => {
+	console.log(`App listening on port ${port}`);
+});
